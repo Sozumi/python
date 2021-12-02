35f108d6ce2bd00eaddd37bdcc1b9137b1ca856c
Sozumi <95422875+Sozumi@users.noreply.github.com>
2021-12-02 19:33:41 +0300

    Delete hw directory

--- hw/hw1/Nikita_Kreymer_hw_1_1.py
+++
@@ -1,7 +0,0 @@
-time = int(input("Введите время в секундах: "))
-days = time // 86400
-hours = (time % 86400) // 3600
-minutes = ((time % 86400) % 3600) // 60
-seconds = ((time % 86400) % 3600) % 60
-
-print(days, "дней", hours, "часов", minutes, "минут", seconds, "секунд")
--- hw/hw1/Nikita_Kreymer_hw_1_3.py
+++
@@ -1,7 +0,0 @@
-for i in range(1, 101):
-    if (10 < i <15) or (i % 10 in [0, 5, 6, 7, 8, 9]):
-        print(i, "процентов")
-    elif i % 10 == 1:
-        print(i, "процент")
-    else:
-        print(i, "процента")
